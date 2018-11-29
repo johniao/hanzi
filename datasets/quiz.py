9546344bf79bb611722cc757ae8b19618cc62776
jy <johniao@163.com>
2018-11-29 09:30:47 +0800

    w

--- datasets/quiz.py
+++ datasets/quiz.py
@@ -29,11 +29,11 @@ from datasets import dataset_utils
 
 slim = tf.contrib.slim
 
-_FILE_PATTERN = 'pj_vehicle_%s_*.tfrecord'
+_FILE_PATTERN = 'hanzi_%s.tfrecord'
 
-SPLITS_TO_SIZES = {'train': 43971, 'validation': 4885}
+SPLITS_TO_SIZES = {'train': 40000, 'validation': 40000}
 
-_NUM_CLASSES = 764
+_NUM_CLASSES = 100
 
 _ITEMS_TO_DESCRIPTIONS = {
     'image': 'A color image of varying size.',
@@ -71,14 +71,14 @@ def get_split(split_name, dataset_dir, file_pattern=None, reader=None):
 
     keys_to_features = {
         'image/encoded': tf.FixedLenFeature((), tf.string, default_value=''),
-        'image/format': tf.FixedLenFeature((), tf.string, default_value='png'),
-        'image/class/label': tf.FixedLenFeature(
-            [], tf.int64, default_value=tf.zeros([], dtype=tf.int64)),
+        'image/filename': tf.FixedLenFeature((), tf.string, default_value='jpg'),
+        'image/object/class/label': tf.FixedLenFeature(
+            (), tf.int64, default_value=tf.zeros((), dtype=tf.int64)),
     }
 
     items_to_handlers = {
         'image': slim.tfexample_decoder.Image(),
-        'label': slim.tfexample_decoder.Tensor('image/class/label'),
+        'label': slim.tfexample_decoder.Tensor('image/object/class/label'),
     }
 
     decoder = slim.tfexample_decoder.TFExampleDecoder(
